78ef9ab3b944cb593b828d3eb1aef9ce4f7c7280
Dan Gold <tentativechaos@gmail.com>
2019-07-28 15:39:27 -0700

    Basically finished the program. Made a forloop that iterates through the dictionary made out of the csv file, ignoring lines with improper title values. Adds appropriate variables to variables, the plugs the title and other info into a url using string formatting, before plugging all of that into one big string that adds the html tags, again using string formatting to plug everything in.

--- afsParse.py
+++ afsParse.py
@@ -1,6 +1,6 @@
 import re  # Regular expression library, possibly not needed.
 import csv  # CSV Parsing Library
-import urllib  # For formatting data when constructing the url. Will use urllib.parse.quote()
+import urllib.parse as parseURL # For formatting data when constructing the url. Will use urllib.parse.quote()
 
 
 # Example of output format:
@@ -32,6 +32,25 @@ import urllib  # For formatting data when constructing the url. Will use urllib.
 with open('test.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile, fieldnames=['Category*','Title','Description', 'CEUs', 'Start Date', 'End Date', 'Start Time', 'End Time', 'Location*', 'Sponsoring Agency*'])
 
+    for row in reader:
+        # Check for blank/title row, and ignore any such
+        if row['Title'] == '' or row['Title'] == 'Title':
+            continue
+
+        title = row['Title']
+        category = row['Category*'].strip()
+        location = row['Location*'].strip()
+        date = row['Start Date'].strip()
+        startTime = row['Start Time'].strip()
+        endTime = row['End Time'].strip()
+        description = row['Description'].strip()
+
+        parsedTitle = parseURL.quote(title)
+        link = 'https://fostercaretraining.org/registration/?course_email=aevans@afs4kids.org&course_title={parsedTitle}&course_date={date}&course_location={location}'.format(parsedTitle=parsedTitle, date=date, location=location)
+
+        print("""<h3>\n\t{title}\n</h3>\n<p>\n\t{category}<br>\n\t{location}<br>\n\t{date}<br>\n\t{startTime}-{endTime}\n</p>\n<p>\n\t{description}\n</p>\n<p>\n\t<a href=\"{link}\">\n\t\tRegister\n\t</a>\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link))
+        
+        print('\n-------------------------------------------------\n')
 
 
 

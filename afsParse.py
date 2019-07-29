6c80fcb27c59e21b0cfa025f4bcafbc73c46ef67
Dan Gold <tentativechaos@gmail.com>
2019-07-29 00:13:36 -0700

    Added a feature where all data is collected and then everything is coppied to the clipboard when the program is finished running. This changes absolutely nothing if the user prefer to copy straight out of the terminal. However, if the user does not like doing that, then this gives them an option to just paste the entire output into a notepad file when done and then copy/paste from there. To implement this I had to import the pyperclip module. In order to make this work for the end-user without them having the download the pyperclip library manually, I also added in a try/except that automatically installs pyperclip if the user doesn't already have it installed.

--- afsParse.py
+++ afsParse.py
@@ -3,6 +3,17 @@ import csv  # CSV Parsing Library
 import urllib.parse as parseURL # For formatting data when constructing the url. Will use urllib.parse.quote()
 import sys  # So I can grab command line arguments for the file name.
 
+try:
+    import pyperclip  # For outputting data to the clipboard 
+except(ModuleNotFoundError):
+    import subprocess
+    def install(package):
+        subprocess.call([sys.executable, "-m", "pip", "install", package])
+    print('Installing Pyperclip... Please wait...')
+    install('pyperclip')
+    import pyperclip
+
+
 # For optional gui file explorer
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
@@ -59,6 +70,8 @@ with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=['Category*','Title','Description', 'CEUs', 'Start Date', 'End Date', 'Start Time', 'End Time', 'Location*', 'Sponsoring Agency*'])
 
 
+    allResults =''
+
     for row in reader:
         # Check for blank/title row, and ignore any such
         if row['Title'] == '' or row['Title'] == 'Title':
@@ -79,7 +92,16 @@ with open(filename, newline='') as csvfile:
 
         # Finally, create a bunch of html tags with the relevent data dropped in. I've also added lots of newline and tab characters.
         # While not actually necessary to the needs of the end user, I think they will non the less appreciate actually being able to read the output 
+        # We'll also throw in a line break between each set of event html tags so they can be easily distinguished from eachother
         print("""<h3>\n\t{title}\n</h3>\n<p>\n\t{category}<br>\n\t{location}<br>\n\t{date}<br>\n\t{startTime}-{endTime}\n</p>\n<p>\n\t{description}\n</p>\n<p>\n\t<a href=\"{link}\">\n\t\tRegister\n\t</a>\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link))
-        
-        # Finally, we'll add some spacing and a line break in the terminal to seperate each chunck of copy/paste text
         print('\n-------------------------------------------------\n')
+
+        # We'll also add the same thing to our allResults string so that we can output everything to the clipboard at once.
+        # Not that for this, we're swapping all of our \n characters for \r\n, so that if this gets pasted into notepad the newlines are actually recognized.
+        # That wasn't necessary for the terminal output as \n should be recognized there.
+        allResults += """<h3>\r\n\t{title}\r\n</h3>\r\n<p>\r\n\t{category}<br>\r\n\t{location}<br>\r\n\t{date}<br>\r\n\t{startTime}-{endTime}\r\n</p>\r\n<p>\r\n\t{description}\r\n</p>\r\n<p>\r\n\t<a href=\"{link}\">\r\n\t\tRegister\r\n\t</a>\r\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link)
+        allResults += '\r\n-------------------------------------------------\r\n'
+        
+    
+    # Once we're done, copy all data to the clipboard using pyperclip
+    pyperclip.copy(allResults)

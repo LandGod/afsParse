843e8a91b349477b9c1787cda7d222bdb7cc7574
Dan Gold <tentativechaos@gmail.com>
2020-08-20 18:30:10 -0700

    Added additional error handling for un-parseable files.

--- afsParse.py
+++ afsParse.py
@@ -57,45 +57,56 @@ if filename[-4:] != '.csv':
 # we must take care to ignore data we don't need
 with open(filename, newline='') as csvfile:
 
-    # Generate csv dictionary for each row based on supplied column headings. Returns iterable full of dictionaries. 
-    reader = csv.DictReader(csvfile, fieldnames=['Category*','Title','Description', 'CEUs', 'Start Date', 'End Date', 'Start Time', 'End Time', 'Location*', 'Sponsoring Agency*'])
+    try:
+        # Generate csv dictionary for each row based on supplied column headings. Returns iterable full of dictionaries. 
+        reader = csv.DictReader(csvfile, fieldnames=['Category*','Title','Description', 'CEUs', 'Start Date', 'End Date', 'Start Time', 'End Time', 'Location*', 'Sponsoring Agency*'])
 
 
-    allResults =''
+        allResults =''
 
-    for row in reader:
-        # Check for blank/title row, and ignore any such
-        if row['Title'] == '' or row['Title'] == 'Title':
-            continue
-        # For useable rows, put each piece of relevent info into variables
-        print(row['Title'])
-        title = row['Title']
-        category = row['Category*'].strip()
-        location = row['Location*'].strip()
-        date = row['Start Date'].strip()
-        startTime = row['Start Time'].strip()
-        endTime = row['End Time'].strip()
-        description = row['Description'].strip()
+        for row in reader:
+            # Check for blank/title row, and ignore any such
+            if row['Title'] == '' or row['Title'] == 'Title':
+                continue
+            # For useable rows, put each piece of relevent info into variables
+            print(row['Title'])
+            title = row['Title']
+            category = row['Category*'].strip()
+            location = row['Location*'].strip()
+            date = row['Start Date'].strip()
+            startTime = row['Start Time'].strip()
+            endTime = row['End Time'].strip()
+            description = row['Description'].strip()
 
-        # Using the other variables, create the form fill linke using urllib to parse the title and then dropping everything into place
-        parsedTitle = parseURL.quote(title)
-        link = 'https://fostercaretraining.org/registration/?course_email=aevans@afs4kids.org&course_title={parsedTitle}&course_date={date}&course_location={location}'.format(parsedTitle=parsedTitle, date=date, location=location)
+            # Using the other variables, create the form fill linke using urllib to parse the title and then dropping everything into place
+            parsedTitle = parseURL.quote(title)
+            link = 'https://fostercaretraining.org/registration/?course_email=aevans@afs4kids.org&course_title={parsedTitle}&course_date={date}&course_location={location}'.format(parsedTitle=parsedTitle, date=date, location=location)
 
-        # Finally, create a bunch of html tags with the relevent data dropped in. I've also added lots of newline and tab characters.
-        # While not actually necessary to the needs of the end user, I think they will non the less appreciate actually being able to read the output 
-        # We'll also throw in a line break between each set of event html tags so they can be easily distinguished from eachother
-        print("""<h3>\n\t{title}\n</h3>\n<p>\n\t{category}<br>\n\t{location}<br>\n\t{date}<br>\n\t{startTime} - {endTime}\n</p>\n<p>\n\t{description}\n</p>\n<p>\n\t<a href=\"{link}\">Register</a>\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link))
-        print('\n-------------------------------------------------\n')
+            # Finally, create a bunch of html tags with the relevent data dropped in. I've also added lots of newline and tab characters.
+            # While not actually necessary to the needs of the end user, I think they will non the less appreciate actually being able to read the output 
+            # We'll also throw in a line break between each set of event html tags so they can be easily distinguished from eachother
+            print("""<h3>\n\t{title}\n</h3>\n<p>\n\t{category}<br>\n\t{location}<br>\n\t{date}<br>\n\t{startTime} - {endTime}\n</p>\n<p>\n\t{description}\n</p>\n<p>\n\t<a href=\"{link}\">Register</a>\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link))
+            print('\n-------------------------------------------------\n')
 
-        # We'll also add the same thing to our allResults string so that we can output everything to the clipboard at once.
-        # Not that for this, we're swapping all of our \n characters for \r\n, so that if this gets pasted into notepad the newlines are actually recognized.
-        # That wasn't necessary for the terminal output as \n should be recognized there.
-        allResults += """<h3>\r\n\t{title}\r\n</h3>\r\n<p>\r\n\t{category}<br>\r\n\t{location}<br>\r\n\t{date}<br>\r\n\t{startTime} - {endTime}\r\n</p>\r\n<p>\r\n\t{description}\r\n</p>\r\n<p>\r\n\t<a href=\"{link}\">Register</a>\r\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link)
-        allResults += '\r\n-------------------------------------------------\r\n'
+            # We'll also add the same thing to our allResults string so that we can output everything to the clipboard at once.
+            # Not that for this, we're swapping all of our \n characters for \r\n, so that if this gets pasted into notepad the newlines are actually recognized.
+            # That wasn't necessary for the terminal output as \n should be recognized there.
+            allResults += """<h3>\r\n\t{title}\r\n</h3>\r\n<p>\r\n\t{category}<br>\r\n\t{location}<br>\r\n\t{date}<br>\r\n\t{startTime} - {endTime}\r\n</p>\r\n<p>\r\n\t{description}\r\n</p>\r\n<p>\r\n\t<a href=\"{link}\">Register</a>\r\n</p>""".format(title=title, category=category, location=location, date=date, startTime=startTime, endTime=endTime, description=description, parsedTitle=parsedTitle, link=link)
+            allResults += '\r\n-------------------------------------------------\r\n'
+            
         
-    
-    # Once we're done, copy all data to the clipboard using pyperclip
-    pyperclip.copy(allResults)
+        # Once we're done, copy all data to the clipboard using pyperclip
+        pyperclip.copy(allResults)
+
+    # If we encounter an error, inform the user so that they can see what the heck is going on
+    except UnicodeDecodeError as e:
+        print("The program encountered a decoding error while trying to read the file and was unable to continue.")
+        print("The file you are trying to process may be corrupted.")
+
+    except:
+        print("The program encountered an unknown error while reading the file and is unable to continue.")
+        print("*****************************************************************************\n")
+        print(sys.exc_info()[0])
 
     # Keep terminal window open after program is finished, even if program not run from terminal
     input('Press [enter] to exit program...\n')
